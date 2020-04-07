54270be8e7c6520967d62c215143d770d4124570
Pierce Butler <Pierce@Melissas-MacBook-Pro.local>
2020-04-07 12:17:54 -0400

    Created React App

--- p4/StepOutYourDoor/app/controllers/experiences_controller.rb
+++ p4/StepOutYourDoor/app/controllers/experiences_controller.rb
@@ -1,5 +1,5 @@
 class ExperiencesController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_experience, only: [:show, :update, :destroy]
 
   # GET /experiences
   def index
@@ -8,8 +8,45 @@ class ExperiencesController < ApplicationController
     render json: @experiences
   end
 
-  
-    # def user_params
-    #   params.require(:user).permit(:name, :avatar, :email, :password)
-    # end
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      @token = encode({ user_id: @user.id })
+      render json: { user: @user.frontend_data, token: @token }, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:name, :avatar, :email, :password)
+    end
 end
