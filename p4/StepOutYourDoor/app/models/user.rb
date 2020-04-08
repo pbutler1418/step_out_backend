c37339560ffb0c98a0d5301792d54831b6bda3fc
Pierce Butler <Pierce@Melissas-MacBook-Pro.local>
2020-04-08 13:05:32 -0400

    Not happy with client, deleted and going back to work on backend.

--- p4/StepOutYourDoor/app/models/user.rb
+++ p4/StepOutYourDoor/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :experience
+  has_one :bucketlist
 
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
